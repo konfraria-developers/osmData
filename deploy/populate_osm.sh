1c8c8fdbab339e8a2d9f12b5c1244e496f8416e8
Xavier Barnada <xbarnada@gmail.com>
2015-07-11 18:54:00 +0200

    Add clean step

--- deploy/populate_osm.sh
+++ deploy/populate_osm.sh
@@ -3,4 +3,5 @@ wget http://download.geofabrik.de/europe/spain-latest.osm.bz2
 bzip2 -d spain-latest.osm.bze
 osm2pgsql -c -s -d osm spain-latest.osm
 rm spain-latest.osm
+psql -c "DELETE FROM platen_osm_polygon WHERE geom not ST_Intersects(t.geom,geom) FROM( select geom from planet_osm_polygon where admin_level='8' and name ILIKE '$TOWN_NAME') AS t "
 
