0919432a4625e582a932633fb0778243f7233f9a
Xavier Barnada <xbarnada@gmail.com>
2015-11-07 10:16:34 +0100

    Format code

--- src/main.py
+++ src/main.py
@@ -2,19 +2,21 @@ from flask import Flask
 app = Flask(__name__)
 app.debug = True
 
+
 @app.route('/hash/<string:population>/<string:dataset>')
-def get_hash(population,dataset):
+def get_hash(population, dataset):
     import os
-    dir = os.path.join(population, str(dataset)+".hash")
+    dir = os.path.join(population, '{}.hash'.format(dataset))
     f = open(dir)
     data = f.read()
     f.close()
     return data
 
+
 @app.route('/data/<string:population>/<string:dataset>')
 def get_data(population, dataset):
     import os
-    dir = os.path.join(population, str(dataset)+".json")
+    dir = os.path.join(population, '{}.json'.format(dataset))
     f = open(dir)
     data = f.read()
     f.close()
